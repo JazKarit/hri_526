4b27eaf09d4f6f008802531a5572fac33cf8cb2b
jazKarit <jaskrits.singh@gmail.com>
2024-11-25 10:25:14 -0500

    Added Wipe and Insert Structs

--- Assets/Scripts/TaskManager.cs
+++ Assets/Scripts/TaskManager.cs
@@ -5,16 +5,85 @@ using RosMessageTypes.Std;
 using Unity.Robotics.ROSTCPConnector;
 using System.Diagnostics;
 
-public class Metrics
+struct Wipe
 {
-    // One csv wipe
-    // one csv insert
-    // each trial is a row
+    // Fields
+    public string InterfaceType;
+    public string Shape;
+    public TimeSpan Time;
+    public string Name;
+    public int GoodPoints;
+    public int BadPoints;
+
+    // Constructor
+    public Wipe(string interfaceType, string shape, TimeSpan time, string name, int goodPoints, int badPoints)
+    {
+        InterfaceType = interfaceType;
+        Shape = shape;
+        Time = time;
+        Name = name;
+        GoodPoints = goodPoints;
+        BadPoints = badPoints;
+    }
+
+    // Method to display wipe information
+    public void DisplayWipeInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Interface Type: {InterfaceType}");
+        Console.WriteLine($"Shape: {Shape}");
+        Console.WriteLine($"Time: {Time.Hours} hours {Time.Minutes} minutes {Time.Seconds} seconds");
+        Console.WriteLine($"Good Points: {GoodPoints}");
+        Console.WriteLine($"Bad Points: {BadPoints}");
+    }
+}
+
+struct Insert
+{
+    // Fields
+    public string ConstraintType;
+    public int PegSize;
+    public string PegColor;
+    public string Name;
+    public TimeSpan Time;
+    public int Collisions;
+    public int Resets;
+    public int Success;
+    public int ConstraintModeToggles;
+
+    // Constructor
+    public Insert(string constraintType, int pegSize, string pegColor, string name, TimeSpan time, int collisions, int resets, int success, int constraintModeToggles)
+    {
+        ConstraintType = constraintType;
+        PegSize = pegSize;
+        PegColor = pegColor;
+        Name = name;
+        Time = time;
+        Collisions = collisions;
+        Resets = resets;
+        Success = success;
+        ConstraintModeToggles = constraintModeToggles;
+    }
+
+    // Method to display insert information
+    public void DisplayInsertInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Constraint Type: {ConstraintType}");
+        Console.WriteLine($"Peg Size: {PegSize}");
+        Console.WriteLine($"Peg Color: {PegColor}");
+        Console.WriteLine($"Time: {Time.Hours} hours {Time.Minutes} minutes {Time.Seconds} seconds");
+        Console.WriteLine($"Collisions: {Collisions}");
+        Console.WriteLine($"Resets: {Resets}");
+        Console.WriteLine($"Success: {Success}");
+        Console.WriteLine($"Constraint Mode Toggles: {ConstraintModeToggles}");
+    }
 }
 
 public class TaskManager : MonoBehaviour
 {
-    public enum TaskState {
+    public enum TaskState
+    {
         IDLE,
         WIPE_POLYGON,
         WIPE_MANUAL,
@@ -33,9 +102,9 @@ public class TaskManager : MonoBehaviour
     public GameObject wipeSurface;
 
     private Stopwatch sw;
-    
-    
-    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -51,10 +120,11 @@ public class TaskManager : MonoBehaviour
 
     }
 
-    public void CommandRecived(StringMsg msg) {
+    public void CommandRecived(StringMsg msg)
+    {
         string[] args = msg.data.Split(' ');
         //Debug.Log(msg.data);
-        
+
 
         //Sorry about this v
 
@@ -64,23 +134,23 @@ public class TaskManager : MonoBehaviour
         if (args[0].Equals("wipe")) goto Wipe;
         //Insertion
         if (args[0].Equals("manual")) goto Insertion_Manual;
-        
+
         //Automatic insertion
         motionConstrainer.constrained = true;
         goto Insertion_Common;
 
-        Insertion_Manual:
+    Insertion_Manual:
         //Manual Insertion
         motionConstrainer.constrained = false;
-        
-        Insertion_Common:
+
+    Insertion_Common:
         sw.Restart();
         EEF.SetActive(true);
         pegBox.SetActive(true);
         state = TaskState.INSERT;
         return;
 
-        Wipe:
+    Wipe:
         sw.Restart();
         DirtPointManager.WipePointMode wm;
         if (args[1].Equals("manual")) goto Wipe_Manual;
@@ -88,43 +158,45 @@ public class TaskManager : MonoBehaviour
         wipeSurface.SetActive(true);
         WipePoint.uiPoint.SetActive(true);
         state = TaskState.WIPE_POLYGON;
-        
+
         wm = DirtPointManager.WipePointMode.SURFACE;
         goto Wipe_General;
-        Wipe_Manual:
+    Wipe_Manual:
         EEF.SetActive(true);
         motionConstrainer.constrained = false;
         wm = DirtPointManager.WipePointMode.EEF;
         state = TaskState.WIPE_MANUAL;
-        Wipe_General:
-        switch (args[2]) {
-            case "square" :
+    Wipe_General:
+        switch (args[2])
+        {
+            case "square":
                 wipeSquare.SetActive(true);
                 wipeSquare.transform.position = positionSquare.transform.position;
                 wipeSquare.GetComponent<DirtPointManager>().Unwipe();
                 wipeSquare.GetComponent<DirtPointManager>().mode = wm;
-            break;
+                break;
             case "pentagon":
                 wipePentagon.SetActive(true);
                 wipePentagon.transform.position = positionPentagon.transform.position;
                 wipePentagon.GetComponent<DirtPointManager>().Unwipe();
                 wipePentagon.GetComponent<DirtPointManager>().mode = wm;
-            break;
+                break;
             case "l":
                 wipeL.SetActive(true);
                 wipeL.transform.position = positionL.transform.position;
                 wipeL.GetComponent<DirtPointManager>().Unwipe();
                 wipeL.GetComponent<DirtPointManager>().mode = wm;
-            break;
+                break;
         }
         return;
 
-        Stop:
+    Stop:
         sw.Stop();
-        switch (state) {
+        switch (state)
+        {
             case TaskState.IDLE:
 
-            break;
+                break;
             case TaskState.WIPE_MANUAL:
 
                 wipeL.SetActive(false);
@@ -132,7 +204,7 @@ public class TaskManager : MonoBehaviour
                 wipePentagon.SetActive(false);
                 EEF.SetActive(false);
 
-            break;
+                break;
             case TaskState.WIPE_POLYGON:
 
                 wipeL.SetActive(false);
@@ -140,29 +212,30 @@ public class TaskManager : MonoBehaviour
                 wipePentagon.SetActive(false);
                 wipeSurface.GetComponent<MeshFilter>().mesh.Clear();
                 wipeSurface.SetActive(false);
-                
-                foreach (GameObject wipePoint in WipePoint.points) {
+
+                foreach (GameObject wipePoint in WipePoint.points)
+                {
                     Destroy(wipePoint);
                 }
                 WipePoint.points.Clear();
                 WipePoint.uiPoint.SetActive(false);
 
-            break;
+                break;
             case TaskState.INSERT:
                 EEF.SetActive(false);
                 pegBox.SetActive(false);
                 motionConstrainer.startNone();
-            break;
+                break;
         }
         state = TaskState.IDLE;
         //Debug.Log("stop");
-        
-         
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
